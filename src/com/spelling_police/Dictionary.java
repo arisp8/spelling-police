2592acb4aa1ead795513588ced35ce060a8bed1b
Aris Pattakos <aris.pattakos@gmail.com>
2017-10-28 20:05:35 +0300

    Create fuzzy search algorithm for finding similar words

--- src/com/spelling_police/Dictionary.java
+++ src/com/spelling_police/Dictionary.java
@@ -52,9 +52,79 @@ public class Dictionary {
 		}
 	}
 	
+	private static int levenshteinDistance(String src, String dest)
+	{
+	    int[][] d = new int[src.length() + 1][dest.length() + 1];
+	    int i, j, cost;
+	    char[] str1 = src.toCharArray();
+	    char[] str2 = dest.toCharArray();
+
+	    for (i = 0; i <= str1.length; i++)
+	    {
+	        d[i][0] = i;
+	    }
+	    for (j = 0; j <= str2.length; j++)
+	    {
+	        d[0][j] = j;
+	    }
+	    for (i = 1; i <= str1.length; i++)
+	    {
+	        for (j = 1; j <= str2.length; j++)
+	        {
+
+	            if (str1[i - 1] == str2[j - 1])
+	                cost = 0;
+	            else
+	                cost = 1;
+
+	            d[i][j] =
+	                Math.min(
+	                    d[i - 1][j] + 1,              // Deletion
+	                    Math.min(
+	                        d[i][j - 1] + 1,          // Insertion
+	                        d[i - 1][j - 1] + cost)); // Substitution
+
+	            if ((i > 1) && (j > 1) && (str1[i - 1] == 
+	                str2[j - 2]) && (str1[i - 2] == str2[j - 1]))
+	            {
+	                d[i][j] = Math.min(d[i][j], d[i - 2][j - 2] + cost);
+	            }
+	        }
+	    }
+
+	    return d[str1.length][str2.length];
+	}
+	
+	private List<String> search(String word, double fuzzyness) {    
+		List<String> foundWords = new ArrayList<String>();
+
+		    for (String s : wordList)
+		    {
+		        // Calculate the Levenshtein-distance:
+		        int levenshteinDistance =
+		            levenshteinDistance(word, s);
+
+		        // Length of the longer string:
+		        int length = Math.max(word.length(), s.length());
+
+		        // Calculate the score:
+		        double score = 1.0 - (double)levenshteinDistance / length;
+
+		        // Match?
+		        if (score > fuzzyness)
+		            foundWords.add(s);
+		    }
+		    return foundWords;
+		}
+	
 	public static void main(String[] args) {
 		System.out.println(System.getProperty("user.dir"));
 		Dictionary dict = new Dictionary("el");
+		
+		int dist = levenshteinDistance("Καλημέρα", "Καλυμέρα");
+		List<String> options = dict.search("καλιμέρα", 0.7); 
+		
+		System.out.println(options);
 	}
 		
 }
