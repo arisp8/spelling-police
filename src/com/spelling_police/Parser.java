b5f8445bd30e9b4fb0c6554bb7e241fd9dfb53cc
angelo1998 <l_angelo@outlook.com>
2017-10-30 18:39:13 +0200

    Split text into words

--- src/com/spelling_police/Parser.java
+++ src/com/spelling_police/Parser.java
@@ -1,5 +1,32 @@
 package com.spelling_police;
 
-public class Parser {
+import java.util.Scanner;
 
+/**
+ * Parser splits Strings into individual words and inserts them in a table.
+ * The method returns the table 'word'.
+ */
+public class Parser {
+	
+	//Separate : the method which splits the Strings 
+	public String[] seperate(String period){
+		//Creation of a table the same size as the number of the words in the period.
+		String words[] = period.split("\\s+");
+		
+		//Inserts the words in order into the table.
+		for (int i=0;i<words.length;i++){
+			words[i] = words[i].replaceAll("[^\\w]" , "");
+		}
+		//Returns the table.
+		return words;
+	}
+	
+	public static void main (String args[] ){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Insert Text : ");
+		String period = input.nextLine();
+		Parser obj = new Parser();
+		obj.seperate(period);
+		input.close();
+	}
 }
