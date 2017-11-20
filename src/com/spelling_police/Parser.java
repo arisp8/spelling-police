f41eeb206fcea146a11635bcaf65bc1647327c84
PhilipposPap <32602675+PhilipposPap@users.noreply.github.com>
2017-11-20 22:25:20 +0200

    Change table to ArrayList and create isWord method
    More regular expressions have been added to omit any symbols/elements that are unnecessary for the spelling check

--- src/com/spelling_police/Parser.java
+++ src/com/spelling_police/Parser.java
@@ -1,42 +1,59 @@
 package com.spelling_police;
 
 import java.util.Scanner;
-
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 /**
- * Parser splits Strings into individual words and inserts them in a table.
- * The method returns the table 'word'.
+ * Parser splits Strings into individual words and inserts them in an Arraylist.
+ * The method returns the ArrayList 'words'.
  */
 public class Parser {
-	
+
 	private String language;
-	
+	private static Pattern pattern = Pattern.compile("[0-9.,\\[\\]\\(\\)\\/%«»]+");
+
 	public Parser(String language) {
 		this.language = language;
 	}
-	
-	//Separate : the method which splits the Strings 
-	public String[] seperate(String period){
-		//Creation of a table the same size as the number of the words in the period.
-		String words[] = period.split("\\s+");
-		
-		//Inserts the words in order into the table.
-		for (int i=0;i<words.length;i++){
-			words[i] = words[i].replaceAll("[.!;\\-?:]", "");
+
+	/* isWord: A method to determine if the element in question is a word or not and returns
+	 *those that are.
+	 */
+	private boolean isWord(String element) {
+
+		if (element.length() == 0) {
+			return false;
 		}
-		//Returns the table.
+
+		Matcher matcher = pattern.matcher(element);
+		return !matcher.find();
+	}
+	// Separate : the method which splits the Strings
+	public ArrayList<String> seperate(String period){
+		//Creation of an ArrayList the same size as the number of the words in the period.
+		String elements[] = period.split("\\s+");
+
+		ArrayList<String> words = new ArrayList<String>();
+
+		//Inserts only the word elements in order into the list.
+		for (int i=0; i < elements.length; i++){
+			elements[i] = elements[i].replaceAll("[-.,!;?:\\(\\)\\[\\]]", "");
+
+			if (isWord(elements[i])) {
+				words.add(elements[i]);
+			}
+
+		}
+		//Returns the list.
 		return words;
 	}
-	
+
 	public static void main (String args[] ){
-		Scanner input = new Scanner(System.in);
-		System.out.println("Insert Text : ");
-		String period = input.nextLine();
+		String period = "Γη Από τη Βικιπαίδεια, την ελεύθερη εγκυκλοπαίδεια Γη  Αστρονομικό σύμβολο της Γης Η Γη (αρχαία ελληνικά: Γα?α3 λατινικά: Terra,[4] αγγλικά: Earth) αποτελεί τον 3ο πιο κοντινό πλανήτη στον Ήλιο, τον πιο πυκνό και τον 5ο μεγαλύτερο σε μάζα στο Ηλιακό Σύστημα και ειδικότερα τον μεγαλύτερο ανάμεσα στους γήινους πλανήτες, δηλαδή τους πλανήτες με στερεό φλοιό και το μοναδικό γνωστό ουράνιο σώμα που φιλοξενεί ζωή. Θέλω 200€ και έχει 20°± κελσίου!";
 		Parser obj = new Parser("el");
-		String[] periodSplit = obj.seperate(period);
-		
-		for (int i = 0; i < periodSplit.length; i++) {
-			System.out.println(periodSplit[i]);
-		}
-		input.close();
+		ArrayList<String> periodSplit = obj.seperate(period);
+
+		System.out.println(periodSplit);
 	}
 }
