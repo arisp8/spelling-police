05a6ded939d8f5a3752c2c280bcf0aab5d21be03
Aris Pattakos <aris.pattakos@gmail.com>
2017-11-24 14:36:10 +0200

    Create class to model mistakes

---
+++ src/com/spelling_police/Mistake.java
@@ -0,0 +1,15 @@
+package com.spelling_police;
+
+public class Mistake {
+
+	private String word;
+
+	public Mistake(String word) {
+		this.word = word;
+	}
+
+	public String toString() {
+		return this.word;
+	}
+
+}
