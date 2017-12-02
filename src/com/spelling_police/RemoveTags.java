1c2e5068b6e4d46e7a1c266468b85ff1a3360fbc
Anthi Boziki <32635514+anthim@users.noreply.github.com>
2017-12-02 19:11:46 +0200

    Returns the text without html tags
    Removes the tags from the html text

---
+++ src/com/spelling_police/RemoveTags.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+/**
+ * @class
+ * Removes the tags from the html text
+ */
+
+public class RemoveTags {
+
+		/**
+		 *Removes the tags from the html text
+		 *@return String text
+	 	 */
+
+		public String removeTags(String text) {
+		String txt = text.replaceAll("<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>", " ");
+		txt = text.replaceAll("\\<[^>]*>","");
+		return txt;
+		}
+}
