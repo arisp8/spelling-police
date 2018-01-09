7fa8fc1e7a653d10fa9a087305c8d4670ba113d7
Philip_Pap <philippapakonstantinou@gmail.com>
2018-01-09 21:04:07 +0200

    Update comments in Parser

--- src/com/spelling_police/Parser.java
+++ src/com/spelling_police/Parser.java
@@ -1,29 +1,27 @@
+// Parser splits Strings/texts into their components.
+
 package com.spelling_police;
 
 import java.util.*;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
-/*
- * Parser splits Strings into periods and then the periods into individual words
- * and inserts them in an Arraylist of ArrayLists.That means that every ArrayList represents
- * a period of the original text and every element in it represents a words in that period.
- */
+
 
 public class Parser {
 
 	private static Pattern pattern = Pattern.compile("^[^A-Za-zΑ-Ωα-ωά-ώΐϊϋ\\p{P}]{1,}$");
 	private static Pattern endOfSentence = Pattern.compile("([\\.!\\?;]){1,}(\\s+[Α-ΖΑ-Ω]|$)");
 	private static Pattern acronyms = Pattern.compile("(?:[a-zA-Zα-ωΑ-Ω]\\.){2,}");
-/*
- * Separates a given text/string into individual periods and
- * inserts them in an ArrayList.
+
+ /*Separates the given text/string into  periods and
+ * inserts each one in an ArrayList.
  */
 	protected ArrayList<String> splitIntoSentences(String text) {
 		ArrayList<String> sentences = new ArrayList<String>();
 		Matcher match = endOfSentence.matcher(text);
 
 		int last_index = 0;
-		
+
 		while (match.find()) {
 			sentences.add(text.substring(last_index, match.start()));
 			last_index = match.start() + 1;
@@ -35,44 +33,42 @@ public class Parser {
 
 		return sentences;
 	}
-	
-	/* 
-	 * Determines if the element in question is a word or not and returns
-	 * true or false depending on the outcome.
+
+	 /*Checks if the element in question is a single word and returns
+	 * 'true' if it is and 'false' otherwise so that it can be excluded from the'result' ArrayList.
 	 */
 	public boolean isWord(String element) {
-		
+
 		if (element.length() == 0) {
 			return false;
 		}
-		
+
 		if (acronyms.matcher(element).find()) {
 			return false;
 		}
-		
+
 		if (pattern.matcher(element).find()) {
 			return false;
 		}
-		
+
 		return true;
 	}
 
-	/*
-	 * separate: A public method that splits every period of the given text into individual words and
-	 * inserts them,after using the 'isWord' method in the ArrayList of ArrayLists 'result'.
-	 */
+ 	  /* Splits every period of the given text into individual words and
+	  * inserts them,if the output of the 'isWord' method is 'true', in the ArrayList of ArrayLists 'result'.
+	  */
 	public ArrayList<ArrayList<String>> separate(String text){
-		
+
 		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
-		
+
 		ArrayList<String> sentences = this.splitIntoSentences(text);
 		for (String sentence : sentences) {
-			
+
 			String[] elements  = sentence.split("\\s+");
 			ArrayList<String> words = new ArrayList<String>();
-			
+
 			for (int i=0; i < elements.length; i++){
-				
+
 				// Having asserted that the current element is not an acronym, it's safe now to remove
 				// any punctuation.
 				if (isWord(elements[i])) {
@@ -82,7 +78,7 @@ public class Parser {
 			}
 			result.add(words);
 		}
-		
+
 		return result;
 	}
 }
