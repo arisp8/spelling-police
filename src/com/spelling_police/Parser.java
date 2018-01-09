cd102e4e3bd798867c1f7821129a6afee72b3a1c
Philip_Pap <philippapakonstantinou@gmail.com>
2018-01-09 21:49:37 +0200

    Correct javadoc comments

--- src/com/spelling_police/Parser.java
+++ src/com/spelling_police/Parser.java
@@ -1,21 +1,23 @@
-// Parser splits Strings/texts into their components.
-
 package com.spelling_police;
-
 import java.util.*;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
-
+/**
+* @class Parser splits Strings/texts into their components.
+*/
 public class Parser {
 
 	private static Pattern pattern = Pattern.compile("^[^A-Za-zΑ-Ωα-ωά-ώΐϊϋ\\p{P}]{1,}$");
 	private static Pattern endOfSentence = Pattern.compile("([\\.!\\?;]){1,}(\\s+[Α-ΖΑ-Ω]|$)");
 	private static Pattern acronyms = Pattern.compile("(?:[a-zA-Zα-ωΑ-Ω]\\.){2,}");
 
- /*Separates the given text/string into  periods and
- * inserts each one in an ArrayList.
- */
+ 	/**
+ 	* Separates the given text/string into  periods and
+ 	* returns an ArrayList containing the period.
+ 	* @param text This is the given text
+ 	* @return sentences ArrayList This the Arraylist containin the sentence
+ 	*/
 	protected ArrayList<String> splitIntoSentences(String text) {
 		ArrayList<String> sentences = new ArrayList<String>();
 		Matcher match = endOfSentence.matcher(text);
@@ -34,9 +36,11 @@ public class Parser {
 		return sentences;
 	}
 
-	 /*Checks if the element in question is a single word and returns
-	 * 'true' if it is and 'false' otherwise so that it can be excluded from the'result' ArrayList.
-	 */
+	/**
+	* Checks if the element in question is a  word
+	* @param element This is the element of the period about to be checked
+	* @return boolean This returns 'true' if the element is a word or 'false' otherwise
+	*/
 	public boolean isWord(String element) {
 
 		if (element.length() == 0) {
@@ -54,9 +58,11 @@ public class Parser {
 		return true;
 	}
 
- 	  /* Splits every period of the given text into individual words and
-	  * inserts them,if the output of the 'isWord' method is 'true', in the ArrayList of ArrayLists 'result'.
-	  */
+ 	/**
+ 	*Splits every period of the given text into individual words
+	*@param text The given text
+     @return ArrayList<ArrayList<String>> This returns the parsed text
+	*/
 	public ArrayList<ArrayList<String>> separate(String text){
 
 		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
