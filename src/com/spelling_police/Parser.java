58c5fece6e893c1a2e73e5169d183578b48e44bc
Philip_Pap <philippapakonstantinou@gmail.com>
2018-01-09 21:12:18 +0200

    Revert "Fix Coments(first time)"
    This reverts commit 28c3f57f03f2000af870376e15234ab11ee27642.

--- src/com/spelling_police/Parser.java
+++ src/com/spelling_police/Parser.java
@@ -1,4 +1,4 @@
-//Parser splits Strings/texts into their components.
+// Parser splits Strings/texts into their components.
 
 package com.spelling_police;
 
@@ -13,10 +13,9 @@ public class Parser {
 	private static Pattern endOfSentence = Pattern.compile("([\\.!\\?;]){1,}(\\s+[Α-ΖΑ-Ω]|$)");
 	private static Pattern acronyms = Pattern.compile("(?:[a-zA-Zα-ωΑ-Ω]\\.){2,}");
 
- 	/**
- 	*Separates the given text/string into  periods and
- 	* inserts each one in an ArrayList.
- 	*/
+ /*Separates the given text/string into  periods and
+ * inserts each one in an ArrayList.
+ */
 	protected ArrayList<String> splitIntoSentences(String text) {
 		ArrayList<String> sentences = new ArrayList<String>();
 		Matcher match = endOfSentence.matcher(text);
@@ -35,10 +34,9 @@ public class Parser {
 		return sentences;
 	}
 
-	/**
-	*Checks if the element in question is a single word and returns
-	* 'true' if it is and 'false' otherwise so that it can be excluded from the'result' ArrayList.
-	*/
+	 /*Checks if the element in question is a single word and returns
+	 * 'true' if it is and 'false' otherwise so that it can be excluded from the'result' ArrayList.
+	 */
 	public boolean isWord(String element) {
 
 		if (element.length() == 0) {
@@ -56,9 +54,9 @@ public class Parser {
 		return true;
 	}
 
- 	/** Splits every period of the given text into individual words and
-	* inserts them,if the output of the 'isWord' method is 'true', in the ArrayList of ArrayLists 'result'.
-	*/
+ 	  /* Splits every period of the given text into individual words and
+	  * inserts them,if the output of the 'isWord' method is 'true', in the ArrayList of ArrayLists 'result'.
+	  */
 	public ArrayList<ArrayList<String>> separate(String text){
 
 		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
