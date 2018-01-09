844fc5349e03a420ab1b6fc4bcfb4c7a6d2f50aa
EiriniPit <eirinipit98@gmail.com>
2018-01-09 19:06:46 +0200

    Junit test for SpellChecker

---
+++ src/com/spelling_police/SpellCheckerTest.java
@@ -0,0 +1,22 @@
+package com.spelling_police;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+class SpellCheckerTest {
+
+	@Test
+	void spellCheckerFindsMistakeInString() {
+		SpellChecker test = new SpellChecker();
+		ArrayList<Mistake> output = test.findMistakes("αβγδ");
+		ArrayList<Mistake> list1 = new ArrayList<Mistake>();
+		list1.add(new Mistake("αβγδ", "el", 1, 1));	
+		
+		assertArrayEquals(list1.toArray(),output.toArray());
+		
+	}
+
+}
