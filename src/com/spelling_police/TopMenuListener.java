dc64e50dc8930b5bac60c09ea7c0b586dcdcac28
johnnyrev <john_revithis@hotmail.com>
2018-01-10 02:35:53 +0200

    Documentation of the TopMenuListener class

--- src/com/spelling_police/TopMenuListener.java
+++ src/com/spelling_police/TopMenuListener.java
@@ -23,27 +23,34 @@ public class TopMenuListener implements ActionListener {
 	private EmptyBorder originalBorder;
 	
 	/**
-	 * constructor
-	 * @param label
+	 * Default constructor of TopMenuListener class
+	 * @param applWind : ApplicationWindow's object
 	 */
 	public TopMenuListener(ApplicationWindow applWind) {
 		
 		this.applWind = applWind;
 		
 	}
-
+	
+	/**
+	 * Performing the actions of the Menu for each case(choice)
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		switch (e.getActionCommand()) {
+			//first case : creates a new editor page
 			case "New":
 				applWind.createTextEditorPage();
 				break;
+			//second case : open a file
 			case "Open":
 				applWind.loadFileFromSystem();
 				break;
+			//third case : saves the document
 			case "Save":
 				applWind.saveFile();
 				break;
+			//last case: exits the program
 			case "Exit":
 				System.exit(0);
 				break;
